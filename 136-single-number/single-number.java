efdead2d73b1a907c4103b16f3d9a2045cf88c7d
Samriddhi Jaiswal <120332474+sam-riddhi@users.noreply.github.com>
2025-07-05 16:51:52 +0530

    Time: 1 ms (99.90%) | Memory: 46.6 MB (10.50%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int xorr = 0;
+        for(int i = 0; i < n; i++)
+        {
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+}
