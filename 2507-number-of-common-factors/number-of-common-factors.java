3ca3850ab09e32c7a4b50cd066e9db407e2f5e81
Samriddhi Jaiswal <120332474+sam-riddhi@users.noreply.github.com>
2026-05-02 18:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (40.85%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.java
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -1,13 +1,24 @@
 class Solution {
     public int commonFactors(int a, int b) {
+        int g = gcd(a, b);
         int c = 0;
-        for(int i = 1; i <= Math.min(a, b); i++)
+        for(int i = 1; i <= g; i++)
         {
-            if(a%i == 0 && b%i == 0)
+            if(a % i == 0 && b % i == 0)
             {
                 c++;
             }
         }
-        return c;
+       return c;
+    }
+    public int gcd(int m, int n)
+    {
+        while(n != 0)
+        {
+            int temp = n;
+            n = m % n;
+            m = temp;
+        }   
+        return m;
     }
 }
